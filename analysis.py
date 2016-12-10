5789234d52265d4f6ccc2e393dcce2dd686ea234
Vishal Kasliwal <vishal.kasliwal@gmail.com>
2016-12-10 13:09:00 -0500

    Make survival analysis plots

--- analysis.py
+++ analysis.py
@@ -7,11 +7,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pdb
 
+import lifelines
+
 import mpl_settings  # Fancy formatting rules for plots.
 
-fhgt = 10  # Height of figures in inch.
-fwid = 16  # Width of figures in inch.
-DPI = 300  # Dots per inch for plots
 mpl_settings.set_plot_params(useTex=True)  # Make plots look good.
 rows, columns = os.popen('stty size', 'r').read().split()
 np.set_printoptions(linewidth=int(columns) - 2)
@@ -23,6 +22,9 @@ parser.add_argument('-pwd', '--pwd', type=str, default=os.environ['PWD'], help=r
 parser.add_argument('--plot', dest='plot', action='store_true', help=r'Make & show plot?')
 parser.add_argument('--no-plot', dest='plot', action='store_false', help=r'Do not make & show plot?')
 parser.set_defaults(plot=True)
+parser.add_argument('-dpi', '--dpi', type=int, default=300, help=r'Dots Per Inch to save figure at?')
+parser.add_argument('-wdth', '--width', type=int, default=16, help=r'Figure width?')
+parser.add_argument('-hght', '--height', type=int, default=10, help=r'Figure height?')
 parser.add_argument('--stop', dest='stop', action='store_true', help=r'Stop at end?')
 parser.add_argument('--no-stop', dest='stop', action='store_false', help=r'Do not stop at end?')
 parser.set_defaults(stop=True)
@@ -38,6 +40,10 @@ parser.set_defaults(rerun=False)
 parser.add_argument('--ion', dest='ion', action='store_true', help=r'Display plots interactively?')
 parser.add_argument('--no-ion', dest='ion', action='store_false', help=r'Do not display plots interactively?')
 parser.set_defaults(ion=False)
+parser.add_argument('--survival', dest='survival', action='store_true', help=r'Perform survival analysis?')
+parser.add_argument('--no-survival', dest='survival', action='store_false', help=r'Do not perform survival\
+                     analysis?')
+parser.set_defaults(survival=True)
 parser.add_argument('--ratios', dest='ratios', action='store_true', help=r'Compute simple ratios?')
 parser.add_argument('--no-ratios', dest='ratios', action='store_false', help=r'Do not compute simple ratios?')
 parser.set_defaults(ratios=True)
@@ -49,11 +55,14 @@ if args.ion:
 # Read in the data as a pandas DataFrame
 # ------------------------------------------------------------------------------------------------------------
 
-if os.path.isfile(os.path.join(os.path.join(args.pwd, 'data/goodRentalData.pkl'))):
+if os.path.isfile(os.path.join(os.path.join(args.pwd, 'data/analysis.pkl'))):
     print 'Reading in pickled data file...'
-    goodDataFile = pickle.load(open(os.path.join(args.pwd, 'data/goodRentalData.pkl'), 'rb'))
-    numRecords = goodDataFile['Account ID'].count()
+    with open(os.path.join(args.pwd, 'data/analysis.pkl'), 'rb') as inFile:
+        cleanData = pickle.load(inFile)
+        promoList = pickle.load(inFile)
+    numRecords = cleanData['Account ID'].count()
     lastRec = numRecords - 1
+    numPromos = len(promoList)
 else:
     print 'Reading in original excel file...'
     dataFile = pd.read_excel(os.path.join(args.pwd, 'data/TT_Dataset_for_Data_Scientist_Candidate.xlsx'),
@@ -67,7 +76,7 @@ else:
 
     badDate = list()
     durationArr = np.zeros(numRecords, dtype=float)
-    notMovedOutArr = np.zeros(numRecords, dtype=bool)
+    movedOutArr = np.zeros(numRecords, dtype=bool)
     infoStr = 'ID: %d; Rented? %r; ReserveDate: %s; MoveInDate: %s; MoveOutDate %s;\
                Duration (hrs): %f; Promo: %s'
     for recNum in xrange(numRecords):
@@ -102,11 +111,11 @@ else:
                     badDate.append(recNum)
                 # Compute rental duration = move out date - move in date
                 duration = dataFile['Move Out Date'].values[recNum] - dataFile['Move In Date'].values[recNum]
-                notMovedOut = True
+                movedOut = True
             else:
                 # Rental still occupied! Compute rental duration = last reserve date - move in date
                 duration = dataFile['Move In Date'].values[lastRec] - dataFile['Move In Date'].values[recNum]
-                notMovedOut = False
+                movedOut = False
             durationArr[recNum] = duration/np.timedelta64(1, 'D')
             # Really we should check the actual timestamps but this is much simpler.
             if durationArr[recNum] < Overnight:
@@ -117,52 +126,130 @@ else:
                                str(dataFile['Move Out Date'].values[recNum]),
                                durationArr[recNum]*24,
                                dataFile['Promotion Name'].values[recNum])
-            notMovedOutArr[recNum] = notMovedOut
+            movedOutArr[recNum] = movedOut
     dataFile['Rental Duration'] = durationArr
-    dataFile['Not Moved Out?'] = notMovedOutArr
+    dataFile['Moved Out?'] = movedOutArr
     badDate = list(set(badDate))
-    goodDataFile = dataFile.drop(dataFile.index[badDate])
-    numRecords = goodDataFile['Account ID'].count()
+    cleanData = dataFile.drop(dataFile.index[badDate])
+    numRecords = cleanData['Account ID'].count()
     lastRec = numRecords - 1
+
+    # Find all the unique promotions
+    promoList = list(set(cleanData['Promotion Name'].values.tolist()))
+    reOrder = [5, 3, 1, 2, 0, 4]
+    promoList = [promoList[index] for index in reOrder]
+    numPromos = len(promoList)
 
     # Dump the processed data as a pickle file so that we don't have to keep re-doing this
     # --------------------------------------------------------------------------------------------------------
-    pickle.dump(goodDataFile, open(os.path.join(args.pwd, 'data/goodRentalData.pkl'), 'wb'))
+    with open(os.path.join(args.pwd, 'data/analysis.pkl'), 'wb') as outFile:
+        pickle.dump(cleanData, outFile)
+        pickle.dump(promoList, outFile)
 
-# Histogram the rental durations
+# Use suyrvival analysis to study the rental durations since our data is right-censored
 # ------------------------------------------------------------------------------------------------------------
 
-if args.plot:
+if args.survival:
     print 'Making histogram of durations'
     binList = [0.0, 0.1, Overnight, 1.0] + [float(i) for i in xrange(30, 630, 30)]
-    goodData = goodDataFile[(goodDataFile['Rented?'] != 0) & (goodDataFile['Not Moved Out?'] != 0)]
-    histOut = plt.hist(
-        goodData['Rental Duration'].values,
-        bins=binList, normed=False, facecolor='green', alpha=0.75)
-    plt.xlabel(r'Duration (days)')
-    plt.ylabel(r'\# of Rentals')
-    if args.save:
-        plt.savefig('./plots/RentalDurations.jpg', dpi=DPI)
-        if args.pdf:
-            plt.savefig('./plots/RentalDurations.pdf', dpi=DPI)
+    rentedData = cleanData[(cleanData['Rented?'] != 0)]
+    numRented = rentedData['Account ID'].count()
+    Dur = rentedData['Rental Duration']
+    MovedOut = rentedData['Moved Out?']
+
+    # Begin by getting Kaplan-Meier Estimate for the whole population
+    kmf = lifelines.KaplanMeierFitter()
+    kmf.fit(Dur, event_observed=MovedOut)
+    print 'Overall Median Rental Duration:', kmf.median_
+    if args.plot:
+        axAll = kmf.plot()
+        fig = plt.gcf()
+        fig.set_size_inches(args.width, args.height)
+        if args.save:
+            fig.savefig('./plots/All_RentalDurationsSurvival.jpg', dpi=args.dpi)
+            if args.pdf:
+                fig.savefig('./plots/All_RentalDurationsSurvival.pdf', dpi=args.dpi)
+    promoOffered = rentedData['Promotion Name']
+    NP = (promoOffered == 'No Promotion')
+    TMF = (promoOffered == 'Two Months Free')
+    FMF = (promoOffered == 'First Month Free')
+    TwMHO = (promoOffered == 'Two Months Half Off')
+    FMHO = (promoOffered == 'First Month Half Off')
+    kmf.fit(Dur[NP], MovedOut[NP], label='No Promotion')
+    print 'Median Rental Duration when "No Promotion" was offered:', kmf.median_
+    if args.plot:
+        ax = kmf.plot()
+    kmf.fit(Dur[TMF], MovedOut[TMF], label='Two Months Free')
+    print 'Median Rental Duration when "Two Months Free" was offered:', kmf.median_
+    if args.plot:
+        kmf.plot(ax=ax)
+    kmf.fit(Dur[FMF], MovedOut[FMF], label='First Month Free')
+    print 'Median Rental Duration when "First Month Free" was offered:', kmf.median_
+    if args.plot:
+        kmf.plot(ax=ax)
+    kmf.fit(Dur[TwMHO], MovedOut[TwMHO], label='Two Months Half Off')
+    print 'Median Rental Duration when "Two Months Half Off" was offered:', kmf.median_
+    if args.plot:
+        kmf.plot(ax=ax)
+    kmf.fit(Dur[FMHO], MovedOut[FMHO], label='First Month Half Off')
+    print 'Median Rental Duration when "First Month Half Off" was offered:', kmf.median_
+    if args.plot:
+        kmf.plot(ax=ax)
+        fig = plt.gcf()
+        fig.set_size_inches(args.width, args.height)
+        if args.save:
+            fig.savefig('./plots/All_RentalDurationsSurvival_Overlaid.jpg', dpi=args.dpi)
+            if args.pdf:
+                fig.savefig('./plots/All_RentalDurationsSurvival_Overlaid.pdf', dpi=args.dpi)
+    #fig = plt.figure()
+    #ax = fig.add_subplot(111)    # The big subplot
+    for i, promoType in enumerate(promoList):
+        if promoType == 'Three Months Half off':
+            continue
+        ax = plt.subplot(2, 3, i + 1)
+        ix = rentedData['Promotion Name'] == promoType
+        kmf.fit(Dur[ix], MovedOut[ix], timeline=np.linspace(0.0, 500.0, 5000), label=promoType)
+        if args.plot:
+            kmf.plot(ax=ax, legend=False)
+            plt.title(promoType)
+            plt.xlim(0.0, 500.0)
+            plt.ylim(0.0, 1.0)
+    if args.plot:
+        plt.tight_layout()
+        fig = plt.gcf()
+        fig.set_size_inches(args.width, args.height)
+        plt.ylabel('Frac. still renting after $t$ months')
+        if args.save:
+            fig.savefig('./plots/All_RentalDurationsSurvival_Panelled.jpg', dpi=args.dpi)
+            if args.pdf:
+                fig.savefig('./plots/All_RentalDurationsSurvival_Panelled.pdf', dpi=args.dpi)
+
+    if args.plot:
+        histData = cleanData[(cleanData['Rented?'] != 0) & (cleanData['Moved Out?'] != 0)]
+        histOut = plt.hist(
+            histData['Rental Duration'].values,
+            bins=binList, normed=False, facecolor='green', alpha=0.75)
+        plt.xlabel(r'Duration (days)')
+        plt.ylabel(r'\# of Rentals')
+        fig = plt.gcf()
+        fig.set_size_inches(args.width, args.height)
+        if args.save:
+            plt.savefig('./plots/RentalDurations.jpg', dpi=args.dpi)
+            if args.pdf:
+                plt.savefig('./plots/RentalDurations.pdf', dpi=args.dpi)
 
 
 # Estimate very simple probabilities for the rental using ratios
 # ------------------------------------------------------------------------------------------------------------
 if args.ratios:
     print 'Computing simple ratio estimates of probabilities'
-    # First find all the unique promotions
-    promoList = list(set(goodDataFile['Promotion Name'].values.tolist()))
-    reOrder = [5, 3, 1, 2, 0, 4]
-    promoList = [promoList[index] for index in reOrder]
-    numPromos = len(promoList)
 
-    # Now do simple counts to get the raw ratios
+    # Do simple counts to get the raw ratios
     countsArray = np.zeros((numPromos, 2), dtype=float)
     for i in xrange(numRecords):
-        promoNum = promoList.index(goodDataFile['Promotion Name'].values[i])
+        promoNum = promoList.index(cleanData['Promotion Name'].values[i])
         countsArray[promoNum, 1] += 1.0
-        if goodDataFile['Rented?'].values[i] != 0:
+        if cleanData['Rented?'].values[i] != 0:
             countsArray[promoNum, 0] += 1.0
     # Dictionary - key: promoName; value: [raw likelihood, number of datapoints]
     rentedProbs = dict((promo, [countsArray[counter, 0]/countsArray[counter, 1],
@@ -170,7 +257,7 @@ if args.ratios:
                        for counter, promo in enumerate(promoList))
     if args.plot:
         plt.clf()
-        plt.figure(1, figsize=(fwid, fhgt))
+        plt.figure(1, figsize=(args.width, args.height))
         plt.barh(range(len(rentedProbs)), [value[0] for value in rentedProbs.values()], align='center')
         plt.yticks(range(len(rentedProbs)), rentedProbs.keys())
         plt.ylabel(r'Promotion Type')
@@ -180,12 +267,17 @@ if args.ratios:
         ax.tick_params(axis='both', which='major', labelsize=14)
         ax.tick_params(axis='both', which='minor', labelsize=14)
         if args.save:
-            plt.savefig('./plots/RentalProbabilities.jpg', dpi=DPI)
+            plt.savefig('./plots/RentalProbabilities.jpg', dpi=args.dpi)
             if args.pdf:
-                plt.savefig('./plots/RentalProbabilities.pdf', dpi=DPI)
+                plt.savefig('./plots/RentalProbabilities.pdf', dpi=args.dpi)
 
 # Examine the rate v/s the duration
 # ------------------------------------------------------------------------------------------------------------
+
+if args.plot:
+    plt.clf()
+    plt.figure(1, figsize=(args.width, args.height))
+    plt.scatter(cleanData['Rental Duration'].values, cleanData['RentRate'].values)
 
 if args.stop:
     pdb.set_trace()
